84f0d6e2a9c7a02dfb76f77169873d1f362b837f
michal <mf0583220705@gmail.com>
2025-07-01 10:29:15 +0300

    Initial commit - added React and Nodejs projects with uploads

--- Nodejs/express.js
+++ Nodejs/express.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const host = process.env.HOST;
 const documentsRouter = require('./routers/document.router.js');
 const cors = require('cors');
 app.use(cors());
@@ -15,6 +14,19 @@ app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send('An error occurred, please try later...');
 })
+// להגדרת תיקיית build כסטטית
+app.use(express.static(path.join(__dirname, 'build')));
+
+// מסלול API לדוגמה
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+// כל שאר הבקשות יחזירו את index.html (כדי ש־React Router יעבוד)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
