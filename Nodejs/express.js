8c711dd85ed6a372d915208cac3beea23a96c7ce
michal <mf0583220705@gmail.com>
2025-07-01 08:17:38 +0300

    הוספת BASE_URL והסרה של כתובת קבועה

--- Nodejs/express.js
+++ Nodejs/express.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const documentsRouter = require('./routers/document.router.js');
+const cors = require('cors');
+app.use(cors());
+const path = require('path');
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use('/api/document', documentsRouter);
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('An error occurred, please try later...');
+})
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`express server is running at http://localhost:${port}`);
 });
