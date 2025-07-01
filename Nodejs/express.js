a5bb8fbeac99e2ea9382197fea32b4044cd5ffe3
michal <mf0583220705@gmail.com>
2025-07-01 08:14:25 +0300

    עדכון:

--- Nodejs/express.js
+++ Nodejs/express.js
@@ -1,20 +1,11 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const documentsRouter = require('./routers/document.router.js');
-const cors = require('cors');
-app.use(cors());
-const path = require('path');
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json());
-app.use('/api/document', documentsRouter);
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('An error occurred, please try later...');
-})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log(`express server is running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
