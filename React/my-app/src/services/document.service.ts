937901a46769885ea82764dd7c68c850875e5b5d
michal <mf0583220705@gmail.com>
2025-07-02 18:24:12 +0300

    first commit

--- React/my-app/src/services/document.service.ts
+++ React/my-app/src/services/document.service.ts
@@ -7,13 +7,13 @@ export function insertDocument(document:any) {
 }
 
 export function getDocumentId(id:any) {
-  return axios.get(`https://react-node-signature-1.onrender.com/api/document${id}`);
+  return axios.get(`https://react-node-signature-1.onrender.com/api/document/${id}`);
 }
 
 export function sendSignature(data:any) {
-  return axios.post(`https://react-node-signature-1.onrender.com/api/document`, data);
+  return axios.post(`https://react-node-signature-1.onrender.com/api/document/signature`, data);
 }
 
 export function getDocumentForId(id:any) {
-  return axios.get(`https://react-node-signature-1.onrender.com/api/document${id}`);
+  return axios.get(`https://react-node-signature-1.onrender.com/api/document/signature/${id}`);
 }
