1073c161d4182c331006b7c4a2a9e3ae4e08da58
michal <mf0583220705@gmail.com>
2025-07-03 22:01:52 +0300

    first commit

--- React/my-app/src/services/document.service.ts
+++ React/my-app/src/services/document.service.ts
@@ -4,20 +4,20 @@ import { DocumentModel } from '../models/DocumentModel';
 export function insertDocument(document: any) {
   
   console.log(document);
-  // https://react-node-signature-1.onrender.com/api/document
   
-  return axios.post(`http://localhost:3001/api/document`, document, )
+  
+  return axios.post(`https://react-node-signature-1.onrender.com/api/document`, document, )
 
 }
 
 export function getDocumentId(id:any) {
-  return axios.get(`http://localhost:3001/api/document/${id}`);
+  return axios.get(`https://react-node-signature-1.onrender.com/api/document/${id}`);
 }
 
 export function sendSignature(data:any) {
-  return axios.post(`http://localhost:3001/api/document`, data);
+  return axios.post(`https://react-node-signature-1.onrender.com/api/document/signature`, data);
 }
 
 export function getDocumentForId(id:any) {
-  return axios.get(`http://localhost:3001/api/document/${id}`);
+  return axios.get(`https://react-node-signature-1.onrender.com/api/document/signature/${id}`);
 }
