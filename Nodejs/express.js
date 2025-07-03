371b7119cd84d1dcf500e869f5189266468980f8
michal <mf0583220705@gmail.com>
2025-07-04 01:05:26 +0300

    first commit

--- Nodejs/express.js
+++ Nodejs/express.js
@@ -10,28 +10,20 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-console.log('Starting to setup routes');
+// ░░░ 1. API routes – בהתחלה
 app.use('/api/document', documentsRouter);
-console.log('Finished setting up /api/document route');
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('An error occurred, please try later...');
-});
-
+// ░░░ 2. Static React build
 app.use(express.static(path.join(__dirname, '../React/my-app/build')));
 
+// ░░░ 3. React fallback – בסוף!
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../React/my-app/build', 'index.html'));
 });
+
+// ░░░ Start server
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server listening on port ${port}`);
 });
